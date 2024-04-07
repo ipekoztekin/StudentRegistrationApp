337f1b5da99ef6546ac07fa56b585cf3bbd40cd4
ipekoztekin <138534154+ipekoztekin@users.noreply.github.com>
2024-04-08 01:08:45 +0300

    First

---
+++ Form1.Designer.cs
@@ -0,0 +1,310 @@
+﻿namespace StudentRegistrationSystem
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.nametext = new System.Windows.Forms.Label();
+            this.surnametext = new System.Windows.Forms.Label();
+            this.emailtext = new System.Windows.Forms.Label();
+            this.birthday = new System.Windows.Forms.Label();
+            this.name = new System.Windows.Forms.TextBox();
+            this.surname = new System.Windows.Forms.TextBox();
+            this.email = new System.Windows.Forms.TextBox();
+            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
+            this.enrollment = new System.Windows.Forms.Button();
+            this.listing = new System.Windows.Forms.Button();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(168)))), ((int)(((byte)(133)))), ((int)(((byte)(133)))));
+            this.dataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.dataGridView1.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.SingleVertical;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.GridColor = System.Drawing.Color.Moccasin;
+            this.dataGridView1.Location = new System.Drawing.Point(21, 430);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(830, 192);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // nametext
+            // 
+            this.nametext.AutoSize = true;
+            this.nametext.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nametext.Location = new System.Drawing.Point(285, 39);
+            this.nametext.Name = "nametext";
+            this.nametext.Size = new System.Drawing.Size(125, 26);
+            this.nametext.TabIndex = 1;
+            this.nametext.Text = "First Name:";
+            // 
+            // surnametext
+            // 
+            this.surnametext.AutoSize = true;
+            this.surnametext.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.surnametext.Location = new System.Drawing.Point(285, 104);
+            this.surnametext.Name = "surnametext";
+            this.surnametext.Size = new System.Drawing.Size(124, 26);
+            this.surnametext.TabIndex = 2;
+            this.surnametext.Text = "Last Name:";
+            // 
+            // emailtext
+            // 
+            this.emailtext.AutoSize = true;
+            this.emailtext.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.emailtext.Location = new System.Drawing.Point(335, 166);
+            this.emailtext.Name = "emailtext";
+            this.emailtext.Size = new System.Drawing.Size(74, 26);
+            this.emailtext.TabIndex = 3;
+            this.emailtext.Text = "Email:";
+            // 
+            // birthday
+            // 
+            this.birthday.AutoSize = true;
+            this.birthday.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.birthday.Location = new System.Drawing.Point(266, 242);
+            this.birthday.Name = "birthday";
+            this.birthday.Size = new System.Drawing.Size(144, 26);
+            this.birthday.TabIndex = 4;
+            this.birthday.Text = "Date Of Birth:";
+            // 
+            // name
+            // 
+            this.name.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.name.Location = new System.Drawing.Point(496, 45);
+            this.name.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.name.Name = "name";
+            this.name.Size = new System.Drawing.Size(218, 34);
+            this.name.TabIndex = 5;
+            // 
+            // surname
+            // 
+            this.surname.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.surname.Location = new System.Drawing.Point(496, 104);
+            this.surname.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.surname.Name = "surname";
+            this.surname.Size = new System.Drawing.Size(218, 34);
+            this.surname.TabIndex = 6;
+            // 
+            // email
+            // 
+            this.email.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.email.Location = new System.Drawing.Point(496, 166);
+            this.email.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.email.Name = "email";
+            this.email.Size = new System.Drawing.Size(218, 34);
+            this.email.TabIndex = 7;
+            // 
+            // maskedTextBox1
+            // 
+            this.maskedTextBox1.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.maskedTextBox1.Location = new System.Drawing.Point(496, 242);
+            this.maskedTextBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.maskedTextBox1.Mask = "00/00/0000";
+            this.maskedTextBox1.Name = "maskedTextBox1";
+            this.maskedTextBox1.Size = new System.Drawing.Size(218, 34);
+            this.maskedTextBox1.TabIndex = 8;
+            this.maskedTextBox1.ValidatingType = typeof(System.DateTime);
+            // 
+            // enrollment
+            // 
+            this.enrollment.BackColor = System.Drawing.Color.Moccasin;
+            this.enrollment.Location = new System.Drawing.Point(590, 325);
+            this.enrollment.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.enrollment.Name = "enrollment";
+            this.enrollment.Size = new System.Drawing.Size(124, 44);
+            this.enrollment.TabIndex = 9;
+            this.enrollment.Text = "enrollment";
+            this.enrollment.UseVisualStyleBackColor = false;
+            this.enrollment.Click += new System.EventHandler(this.enrollment_Click);
+            // 
+            // listing
+            // 
+            this.listing.BackColor = System.Drawing.Color.Moccasin;
+            this.listing.Location = new System.Drawing.Point(21, 378);
+            this.listing.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.listing.Name = "listing";
+            this.listing.Size = new System.Drawing.Size(89, 44);
+            this.listing.TabIndex = 10;
+            this.listing.Text = "Listing";
+            this.listing.UseVisualStyleBackColor = false;
+            this.listing.Click += new System.EventHandler(this.listing_Click);
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.Image = global::StudentRegistrationSystem.Properties.Resources.wooden_stick;
+            this.pictureBox6.Location = new System.Drawing.Point(188, 88);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(42, 40);
+            this.pictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox6.TabIndex = 16;
+            this.pictureBox6.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Image = global::StudentRegistrationSystem.Properties.Resources.wooden_stick;
+            this.pictureBox5.Location = new System.Drawing.Point(236, 88);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(43, 41);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox5.TabIndex = 15;
+            this.pictureBox5.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = global::StudentRegistrationSystem.Properties.Resources.wooden_stick;
+            this.pictureBox4.Location = new System.Drawing.Point(210, 26);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(50, 44);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 14;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::StudentRegistrationSystem.Properties.Resources.halloween_mail;
+            this.pictureBox3.Location = new System.Drawing.Point(280, 138);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(59, 54);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 13;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::StudentRegistrationSystem.Properties.Resources.halloween_calendar;
+            this.pictureBox2.Location = new System.Drawing.Point(188, 214);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(56, 54);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 12;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::StudentRegistrationSystem.Properties.Resources.bell;
+            this.pictureBox1.Location = new System.Drawing.Point(116, 368);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(55, 53);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 11;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(284, 636);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(308, 31);
+            this.label1.TabIndex = 17;
+            this.label1.Text = "İPEK ÖZTEKİN 222523074\r\n";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.RosyBrown;
+            this.ClientSize = new System.Drawing.Size(872, 703);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox6);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.listing);
+            this.Controls.Add(this.enrollment);
+            this.Controls.Add(this.maskedTextBox1);
+            this.Controls.Add(this.email);
+            this.Controls.Add(this.surname);
+            this.Controls.Add(this.name);
+            this.Controls.Add(this.birthday);
+            this.Controls.Add(this.emailtext);
+            this.Controls.Add(this.surnametext);
+            this.Controls.Add(this.nametext);
+            this.Controls.Add(this.dataGridView1);
+            this.Font = new System.Drawing.Font("Microsoft YaHei", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.Text = "Student Registration";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label nametext;
+        private System.Windows.Forms.Label surnametext;
+        private System.Windows.Forms.Label emailtext;
+        private System.Windows.Forms.Label birthday;
+        private System.Windows.Forms.TextBox name;
+        private System.Windows.Forms.TextBox surname;
+        private System.Windows.Forms.TextBox email;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox1;
+        private System.Windows.Forms.Button enrollment;
+        private System.Windows.Forms.Button listing;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace StudentRegistrationSystem
+{
+    public partial class Form1 : Form
+    {
+        private string connectionString = "Server=DESKTOP-NFUGI3G\\SQLEXPRESS;Database=StudentRegistration;User Id=sa;Password=996633;";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void listing_Click(object sender, EventArgs e)
+        {
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string query = "SELECT * FROM Students";
+                SqlCommand command = new SqlCommand(query, connection);
+                connection.Open();
+
+                DataTable table = new DataTable();
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    table.Load(reader);
+                }
+                dataGridView1.DataSource = table;
+                connection.Close();
+            }
+        }
+
+        private void enrollment_Click(object sender, EventArgs e)
+        {
+            // Girişlerin boş olup olmadığını kontrol edin
+             if (string.IsNullOrWhiteSpace(name.Text) ||
+                 string.IsNullOrWhiteSpace(surname.Text) ||
+                 string.IsNullOrWhiteSpace(email.Text) ||
+                 string.IsNullOrWhiteSpace(maskedTextBox1.Text))
+             {
+                 MessageBox.Show("Lütfen Öğrencimizin Tüm Bilgilerini Doldurunuz veya Hataları Düzeltiniz.", "Eksik veya Hatalı Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                 return; // Eğer herhangi bir alan boş ise işlemi durdur
+             }
+
+             // Öğrencinin var olup olmadığını kontrol etmek için sorguyu oluştur
+             string checkQuery = "SELECT COUNT(*) FROM Students WHERE FirstName = @FirstName AND LastName = @LastName AND Email = @Email AND DateOfBirth = @DateOfBirth";
+
+             // SqlConnection ve SqlCommand nesnelerini oluştur
+             using (SqlConnection connection = new SqlConnection(connectionString))
+             using (SqlCommand checkCommand = new SqlCommand(checkQuery, connection))
+             {
+                 // Parametre ekleyerek güvenli bir sorgu yapın
+                 checkCommand.Parameters.AddWithValue("@FirstName", name.Text);
+                 checkCommand.Parameters.AddWithValue("@LastName", surname.Text);
+                 checkCommand.Parameters.AddWithValue("@Email", email.Text);
+                 checkCommand.Parameters.AddWithValue("@DateOfBirth", DateTime.Parse(maskedTextBox1.Text));
+
+                 // Bağlantıyı açın ve sorguyu çalıştırın
+                 connection.Open();
+                 int count = (int)checkCommand.ExecuteScalar();
+
+                 // Eğer öğrenci varsa uyarı ver ve işlemi durdur
+                 if (count > 0)
+                 {
+                     MessageBox.Show("Bu öğrenci zaten sistemde kayıtlıdır.", "Başarısız", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                     return;
+                 }
+             }
+
+             // Ekleme sorgusunu oluştur
+             string query = "INSERT INTO Students (FirstName, LastName, Email, DateOfBirth, EnrolledDate) VALUES (@FirstName, @LastName, @Email, @DateOfBirth, @EnrolledDate)";
+
+             // SqlConnection ve SqlCommand nesnelerini oluştur
+             using (SqlConnection connection = new SqlConnection(connectionString))
+             using (SqlCommand command = new SqlCommand(query, connection))
+             {
+                 // Parametre ekleyerek güvenli bir sorgu yapın
+                 command.Parameters.AddWithValue("@FirstName", name.Text);
+                 command.Parameters.AddWithValue("@LastName", surname.Text);
+                 command.Parameters.AddWithValue("@Email", email.Text);
+                 command.Parameters.AddWithValue("@DateOfBirth", DateTime.Parse(maskedTextBox1.Text));
+                 command.Parameters.AddWithValue("@EnrolledDate", DateTime.Now); // Günün tarihini al
+
+                 // Bağlantıyı açın ve sorguyu çalıştırın
+                 connection.Open();
+                 command.ExecuteNonQuery();
+             }
+
+             // Ekleme işlemi tamamlandıktan sonra DataGridView'i güncelle
+             listing_Click(sender, e);
+
+             MessageBox.Show("Öğrencimiz Sisteme Başarı İle Kayıt Edilmiştir.", "Başarılı", MessageBoxButtons.OK, MessageBoxIcon.Information);
+         
+        }
+
+    }
+}
